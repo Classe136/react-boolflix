1829d6e9caa85fb2c709e6a980d0618dbe1edfa8
klelia <clelia.fradella@gmail.com>
2025-01-15 11:39:23 +0100

    loader

--- src/components/MainComponent.jsx
+++ src/components/MainComponent.jsx
@@ -1,22 +1,25 @@
-import { useState, useEffect } from "react";
 import { useGlobalContext } from "../contexts/GlobalContext";
+import Loader from "./Loader";
 import MediaList from "./MediaList";
 function MainComponent() {
   const { movies, series, isSearching, loading } = useGlobalContext();
   //console.log(movies);
-  return (
-    <main className="container-fluid">
-      {!isSearching ? (
+  const renderResults = () => {
+    if (!isSearching) {
+      return (
         <div className="d-flex container h-100 align-items-center justify-content-center">
           <h2>Prova a cercare un film o una serie tv!</h2>
         </div>
-      ) : (
-        <>
-          <MediaList title="Movies" list={movies} key="movie" />
-          <MediaList title="TV Series" list={series} key="tv" />
-        </>
-      )}
-    </main>
-  );
+      );
+    }
+    if (isSearching && loading) return <Loader />;
+    return (
+      <>
+        <MediaList title="Movies" list={movies} key="movie" />
+        <MediaList title="TV Series" list={series} key="tv" />
+      </>
+    );
+  };
+  return <main className="container-fluid">{renderResults()}</main>;
 }
 export default MainComponent;
